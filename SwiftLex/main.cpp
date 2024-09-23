a7d02b10892458bfbeacb5006d42a850c574a1cb
MatiasMoore <nikita.birukov.03@mail.ru>
2024-09-23 16:11:33 +0300

    feat: renamed input file

--- SwiftLex/main.cpp
+++ SwiftLex/main.cpp
@@ -5,7 +5,7 @@ extern FILE* yyin;
 
 int main()
 {
-	fopen_s(&yyin, "test.txt", "r");
+	fopen_s(&yyin, "input.txt", "r");
 	if (!yyin)
 		std::cout << "Couldn't open file! Check the path!" << std::endl;
 	yylex();
