ffe1e4fbf878cb9c0ff03837f7513e369a4800d2
T-LORE <tlore007@gmail.com>
2024-09-28 15:51:12 +0300

    fix: remove russian locale

--- SwiftLex/main.cpp
+++ SwiftLex/main.cpp
@@ -5,7 +5,6 @@ extern FILE* yyin;
 
 int main()
 {
-	setlocale(LC_ALL, "russian");
 	fopen_s(&yyin, "input.txt", "r");
 	if (!yyin)
 		std::cout << "Couldn't open file! Check the path!" << std::endl;
