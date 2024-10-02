0493a87d32d570ed49ca619974f80882c8b3b27c
T-LORE <tlore007@gmail.com>
2024-10-02 13:51:19 +0300

    feat: open preset file on debug config

--- SwiftLex/main.cpp
+++ SwiftLex/main.cpp
@@ -3,10 +3,25 @@
 int yylex();
 extern FILE* yyin;
 
-int main()
+int main(int argc, const char* argv[])
 {
+#ifdef  _DEBUG
 	fopen_s(&yyin, "input.txt", "r");
+#else
+	if (argc != 2)
+	{
+		std::cout << "Incorrect amount of args! The only argument is the file name" << std::endl;
+		return 1;
+	}
+
+	fopen_s(&yyin, argv[1], "r");
+#endif //  _DEBUG
+
+	
 	if (!yyin)
+	{
 		std::cout << "Couldn't open file! Check the path!" << std::endl;
+		return 1;
+	}
 	yylex();
 }
