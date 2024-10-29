c78bd993a5d15165b510bfc652017eb9d841452c
MatiasMoore <nikita.birukov.03@mail.ru>
2024-10-29 23:21:57 +0300

    feat: connection with label

--- SwiftLex/nodes/dotHelpers.cpp
+++ SwiftLex/nodes/dotHelpers.cpp
@@ -5,6 +5,11 @@ std::string dotConnection(int id1, int id2)
 	return std::to_string(id1) + "->" + std::to_string(id2) + '\n';
 }
 
+std::string dotConnectionWithLabel(int id1, int id2, std::string text)
+{
+	return std::to_string(id1) + "->" + std::to_string(id2) + " [label=\"" + text + "\"]" + '\n';;
+}
+
 std::string dotLabel(int id, std::string text)
 {
 	return std::to_string(id) + " [label=\"" + text + "\"]" + '\n';
--- SwiftLex/nodes/dotHelpers.h
+++ SwiftLex/nodes/dotHelpers.h
@@ -4,5 +4,7 @@
 
 std::string dotConnection(int id1, int id2);
 
+std::string dotConnectionWithLabel(int id1, int id2, std::string text);
+
 std::string dotLabel(int id, std::string text);
 
