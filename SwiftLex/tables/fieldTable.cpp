19f931f4b6b59bcd79c11e56bc54f091466704cb
T-LORE <tlore007@gmail.com>
2025-01-19 03:25:25 +0300

    refactor: field table moved to a separate file

--- SwiftLex/tables/fieldTable.cpp
+++ SwiftLex/tables/fieldTable.cpp
@@ -1,1 +1,79 @@
 #include "fieldTable.h"
+#include "../ExceptionHelper.h"
+
+ExternalField::ExternalField(std::string varName, std::string descriptor, std::string className)
+{
+	this->_varName = varName;
+	this->_descriptor = descriptor;
+	this->_className = className;
+}
+
+int ExternalField::addFieldRefToConstTable(ConstantTable* constTable)
+{
+	if (this->findFieldRef(constTable) != -1 )
+		throw std::runtime_error("Equal field with name " + this->_varName + " and descriptor " + this->_descriptor + " already exists in this class!" + LINE_AND_FILE);
+
+	int varNameRef = constTable->addUTF8(this->_varName);
+	int descriptorRef = constTable->addUTF8(this->_descriptor);
+	int nameAndTypeRef = constTable->addNameAndType(varNameRef, descriptorRef);
+
+	int classNameRef = constTable->addUTF8(this->_className);
+	int classRef = constTable->addClassRef(classNameRef);
+
+	int fieldRef = constTable->addFieldRef(classRef, nameAndTypeRef);
+
+	return fieldRef;
+}
+
+int ExternalField::findFieldRef(ConstantTable* constTable)
+{
+	int nameRef = constTable->findUTF8(this->_varName);
+	int descriptorRef = constTable->findUTF8(this->_descriptor);
+	int classNameRef = constTable->findUTF8(this->_className);
+	if (nameRef == -1 || descriptorRef == -1 || classNameRef == -1)
+		return -1;
+
+	int nameAndTypeRef = constTable->findNameAndType(nameRef, descriptorRef);
+	int classRef = constTable->findClassRef(classNameRef);
+
+	if (nameAndTypeRef == -1 || classRef == -1)
+		return -1;
+
+	return constTable->findFieldRef(classRef, nameAndTypeRef);
+}
+
+std::string ExternalField::getVarName()
+{
+	return this->_varName;
+}
+
+std::string ExternalField::getClassName()
+{
+	return this->_className;
+}
+
+std::string ExternalField::getDescriptor()
+{
+	return this->_descriptor;
+}
+
+InternalField::InternalField(std::string varName, std::string descriptor, std::string className, std::vector<FieldAccessFlag> flags, ExprNode* constValue) : ExternalField(varName, descriptor, className)
+{
+	this->_constValue = constValue;
+	this->_flags = flags;
+}
+
+int InternalField::accessFlagsToInt(std::vector<FieldAccessFlag> flags)
+{
+	int accessFlag = 0;
+	for (auto& flag : flags)
+	{
+		accessFlag += (int)flag;
+	}
+	return accessFlag;
+}
+
+bool InternalField::isStatic()
+{
+	return std::find(this->_flags.begin(), this->_flags.end(), F_ACC_STATIC) != this->_flags.end();
+}
--- SwiftLex/tables/fieldTable.h
+++ SwiftLex/tables/fieldTable.h
@@ -1,14 +1,53 @@
 #pragma once
+#include <string>
+#include <map>
+#include <vector>
+#include "constTable.h"
+#include "../nodes/VarDeclarationNode.h"
+#include "../nodes/AccessModifierNode.h"
 
 enum FieldAccessFlag {
-    F_ACC_PUBLIC = MethodAccessFlag::M_ACC_PUBLIC,         //	Declared public; may be accessed from outside its package.
-    F_ACC_PRIVATE = MethodAccessFlag::M_ACC_PRIVATE,       //	Declared private; accessible only within the defining class.
-    F_ACC_PROTECTED = MethodAccessFlag::M_ACC_PROTECTED,   //	Declared protected; may be accessed within subclasses.
-    F_ACC_STATIC = MethodAccessFlag::M_ACC_STATIC,         //	Declared static.
-    F_ACC_FINAL = MethodAccessFlag::M_ACC_FINAL,           //	Declared final; must not be overridden(§5.4.5).
-    F_ACC_VOLATILE = 0x0040,                               //   Declared volatile; cannot be cached.
-    F_ACC_TRANSIENT = 0x0080,                              //   Declared transient; not written or read by a persistent object manager.
-    F_ACC_SYNTHETIC = MethodAccessFlag::M_ACC_SYNTHETIC,   //	Declared synthetic; not present in the source code.
-    F_ACC_ENUM = 0x4000                                    //   Declared as an element of an enum class.
+    F_ACC_PUBLIC = 0x0001,         //	Declared public; may be accessed from outside its package.
+    F_ACC_PRIVATE = 0x0002,        //	Declared private; accessible only within the defining class.
+    F_ACC_PROTECTED = 0x0004,      //	Declared protected; may be accessed within subclasses.
+    F_ACC_STATIC = 0x0008,         //	Declared static.
+    F_ACC_FINAL = 0x0010,          //	Declared final; must not be overridden(§5.4.5).
+    F_ACC_VOLATILE = 0x0040,       //   Declared volatile; cannot be cached.
+    F_ACC_TRANSIENT = 0x0080,      //   Declared transient; not written or read by a persistent object manager.
+    F_ACC_SYNTHETIC = 0x1000,      //	Declared synthetic; not present in the source code.
+    F_ACC_ENUM = 0x4000            //   Declared as an element of an enum class.
 };
 
+class ExternalField
+{
+public:
+    ExternalField(std::string varName, std::string descriptor, std::string className);
+
+    int addFieldRefToConstTable(ConstantTable* constTable);
+    int findFieldRef(ConstantTable* constTable);
+    std::string getVarName();
+    std::string getDescriptor();
+    std::string getClassName();
+
+protected:
+    std::string _varName;
+
+    std::string _descriptor;
+
+    std::string _className;
+};
+
+class InternalField : public ExternalField
+{
+public:
+    InternalField(std::string name, std::string descriptorr, std::string className, std::vector<FieldAccessFlag> flags, ExprNode* constValue = nullptr);
+    
+    int accessFlagsToInt(std::vector<FieldAccessFlag> flags);
+
+    bool isStatic();
+
+private:
+    std::vector<FieldAccessFlag> _flags;
+
+    ExprNode* _constValue;
+};
