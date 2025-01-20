b60632a8f761945b83966d8f9301351e72c28254
T-LORE <tlore007@gmail.com>
2025-01-20 04:51:17 +0300

    refactor: add newlines to exeption message

--- SwiftLex/ExceptionHelper.h
+++ SwiftLex/ExceptionHelper.h
@@ -1,3 +1,3 @@
 #pragma once
 #include <stdexcept>
-#define LINE_AND_FILE std::string(" error occured at line ") + std::to_string(__LINE__) + std::string(" in file ") + std::string(__FILE__)
+#define LINE_AND_FILE std::string("\n") + std::string(" error occured at line ") + std::to_string(__LINE__) + std::string(" in file ") + std::string(__FILE__) + std::string("\n\n")
