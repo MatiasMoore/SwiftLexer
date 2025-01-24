46d35c9c617f636728b8e7c21ac2c001ba181a51
MatiasMoore <nikita.birukov.03@mail.ru>
2025-01-24 04:32:43 +0300

    feat: release configuration

--- SwiftLex/ExceptionHelper.h
+++ SwiftLex/ExceptionHelper.h
@@ -1,3 +1,10 @@
 #pragma once
 #include <stdexcept>
+
+#ifdef  _DEBUG
 #define LINE_AND_FILE std::string("\n") + std::string(" error occured at line ") + std::to_string(__LINE__) + std::string(" in file ") + std::string(__FILE__) + std::string("\n\n")
+#else
+#define LINE_AND_FILE std::string("")
+
+#endif //  _DEBUG
+
